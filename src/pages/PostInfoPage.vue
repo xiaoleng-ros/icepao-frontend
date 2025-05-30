6814080ec2cd1caf722edd140664270fcf057625
xiaoleng <1873048956@qq.com>
2025-05-30 15:18:14 +0800

    fix:优化评论时需手动刷新的问题

--- src/pages/PostInfoPage.vue
+++ src/pages/PostInfoPage.vue
@@ -1,4 +1,5 @@
 <template>
+  <div>
     <van-cell-group inset>
       <van-card
           :title="post.username"
@@ -8,7 +9,6 @@
           <div style="margin-top: 2px;color: darkgray">
             {{ post.content }}
           </div>
-
         </template>
         <template #footer>
           <van-divider/>
@@ -23,31 +23,32 @@
       <div
           id="postComment"
           v-for="comment in post.postCommentList"
+          :key="comment.id"
       >
         <van-cell :title="comment.username"
                   :icon="comment.avatarUrl"
                   :label="comment.content">
         </van-cell>
-
       </div>
     </van-cell-group>
     <van-divider :style="{ color: '#1989fa', borderColor: '#1989fa', padding: '0 16px' }"/>
-  <van-empty v-if="!post || post.length<1" description="帖子不存在"/>
-  <van-action-sheet v-model:show="show" title="评论" @close="doAddCommentCancel">
-    <van-cell-group inset>
-      <van-field
-          v-model="comment"
-          rows="2"
-          autosize
-          type="textarea"
-          maxlength="200"
-          placeholder="请输入评论"
-          show-word-limit
-      />
-    </van-cell-group>
-    <van-button size="small" type="primary" style="float: right; margin: 5px 12px;" @click="doAddComment">发送
-    </van-button>
-  </van-action-sheet>
+    <van-empty v-if="!post || post.length<1" description="帖子不存在"/>
+    <van-action-sheet v-model:show="show" title="评论" @close="doAddCommentCancel">
+      <van-cell-group inset>
+        <van-field
+            v-model="comment"
+            rows="2"
+            autosize
+            type="textarea"
+            maxlength="200"
+            placeholder="请输入评论"
+            show-word-limit
+        />
+      </van-cell-group>
+      <van-button size="small" type="primary" style="float: right; margin: 5px 12px;" @click="doAddComment">发送
+      </van-button>
+    </van-action-sheet>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -56,7 +57,6 @@ import myAxios from "../plugins/myAxios";
 import {showFailToast, showSuccessToast} from "vant";
 import {useRoute, useRouter} from "vue-router";
 import {getCurrentUser} from "../services/user";
-
 
 const currentUser = ref('');
 const comment = ref('');
@@ -71,8 +71,8 @@ const route = useRoute();
  * 接收帖子的信息
  */
 onMounted(async () => {
-  postId.value  = route.query.id;
-  console.log("postId",post.value)
+  postId.value = route.query.id;
+  console.log("postId", post.value)
   await getPost();
   currentUser.value = await getCurrentUser();
 })
@@ -86,24 +86,27 @@ const doDeletePost = async (id: number) => {
   })
   if (res?.code === 0 && res) {
     showSuccessToast("删除成功")
+    // 删除成功后返回上一页
+    router.back()
   } else {
     showFailToast('删除失败' + (res.description ? `,${res.description}` : ''))
   }
 }
+
 /**
- * 获取帖子的列表
+ * 获取帖子的详细信息
  */
 const getPost = async () => {
   const res = await myAxios.get("/post/get", {
     params: {
-      id : postId.value,
+      id: postId.value,
     }
   });
   if (res) {
     post.value = res.data;
     console.log(post)
   } else {
-    showFailToast("加载队伍失败")
+    showFailToast("加载帖子失败")
   }
 };
 
@@ -121,14 +124,17 @@ const doAddComment = async () => {
     postId: addCommentPost.value,
   })
   if (res?.code === 0 && res) {
+    showSuccessToast('评论成功')
     doAddCommentCancel()
+    // 重新获取帖子数据以显示最新评论
+    await getPost()
   } else {
     showFailToast('评论失败' + (res.description ? `,${res.description}` : ''))
   }
 }
 
 /**
- * 关闭评论
+ * 关闭评论弹窗
  */
 const doAddCommentCancel = () => {
   comment.value = '';
@@ -136,7 +142,7 @@ const doAddCommentCancel = () => {
 }
 
 /**
- * 点赞
+ * 点赞功能
  */
 const doThumb = async (postId: number) => {
   const res = await myAxios.post("/post_thumb/", {
@@ -144,10 +150,10 @@ const doThumb = async (postId: number) => {
   })
   if (res?.code === 0 && res) {
     //返回为1表示点赞 返回为-1表示取消点赞
-    if (res.data === 1){
-      post.value.thumbNum +=1;
-    }else {
-      post.value.thumbNum -=1;
+    if (res.data === 1) {
+      post.value.thumbNum += 1;
+    } else {
+      post.value.thumbNum -= 1;
     }
     post.value.thumb = !post.value.thumb;
   } else {
@@ -161,5 +167,4 @@ watchEffect(() => {
 </script>
 
 <style scoped>
-
 </style>
